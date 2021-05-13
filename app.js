5a4c2ed8624938edd5241fd290513414dac9061b
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-12 16:47:48 -1000

    input updates

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 "use strict";
 
 let cmdInput = document.getElementById("cmdline-input");
+let myOutput = document.getElementsByClassName("output-container")[0];
 
-cmdInput.addEventListener("input", (e) => {
-  console.log(cmdInput.value);
-});
+const createOutput = (e) => {
+  if (e.code === "Enter") {
+    // console.log(e.target.value.toLowerCase());
+
+    if (e.target.value.toLowerCase() === "clear") {
+      myOutput.innerHTML = "";
+    } else {
+      let newElem = document.createElement("div");
+      newElem.classList.add("prompt");
+      newElem.innerHTML = `KenjiNozaki@gmail.com: ${e.target.value}`;
+      myOutput.appendChild(newElem);
+      // console.log(myOutput)
+    }
+    
+    e.target.value = "";
+  }
+
+  if (e.code === "Tab") {
+    console.log(e.target.value);
+    e.target.value = "";
+  }
+};
+
+cmdInput.addEventListener("keydown", createOutput);
